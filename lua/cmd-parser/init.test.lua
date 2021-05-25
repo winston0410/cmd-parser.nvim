2479676fe631992ef0baa219ca675d4d70ba928a
kghugo <hugosum.dev@protonmail.com>
2021-05-25 14:12:07 +0800

    Test if no range is included in the command

--- lua/cmd-parser/init.test.lua
+++ lua/cmd-parser/init.test.lua
@@ -11,6 +11,12 @@ describe('when parse_cmd is called', function()
        function() expect.equal(type(result), "table") end)
 end)
 
+describe('when it is called without range', function()
+	local result = parse_cmd("d")
+	it('should return empty start range',
+	function() expect.equal(result.start_range, nil) end)
+end)
+
 describe('when it is called with single number range', function()
     local result = parse_cmd("23d")
     it('should return the start range',
